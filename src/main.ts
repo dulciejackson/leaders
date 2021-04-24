cbfd54970af497764174c7c6c6dae9c5aac4e9df
Dulcie <dulciejackson@gmail.com>
2021-04-24 12:01:20 +0100

    Prismic integration setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import PrismicVue from '@prismicio/vue'
 
 Vue.config.productionTip = false
+
+const endpoint = process.env.VUE_APP_ENDPOINT
+const accessToken = process.env.VUE_APP_ACCESS_TOKEN
+
+// Register plugin
+Vue.use(PrismicVue, {
+  endpoint,
+  apiOptions: { accessToken }
+})
 
 new Vue({
   render: h => h(App),
