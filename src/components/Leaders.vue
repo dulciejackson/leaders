6f8da6acfedc011e756898932514be303a916291
Dulcie <dulciejackson@gmail.com>
2021-04-24 18:29:11 +0100

    Adds collabsible sections to cards

--- src/components/Leaders.vue
+++ src/components/Leaders.vue
@@ -3,22 +3,29 @@
     <prismic-rich-text :field="document.data.title" class="title"/>
 
     <div class="card-group">
-        <div v-for="type in document.data.types" :key="type.name.text" class="leader-card" v-bind:style="{'--leader-colour': type.colour}">
-            <prismic-image :field="type.exemplar_image" class="leader-img"/>
-            <prismic-rich-text :field="type.name" class="leader-name"/>
+      <div v-for="(type, index) in document.data.types" :key="type.name.text" class="leader-card" v-bind:style="{'--leader-colour': type.colour}"> 
+
+        <input :id="'leader-card-' + index" class="toggle" type="checkbox"> 
+        <label :for="'leader-card-' + index" class="card-header">
+          <prismic-image :field="type.exemplar_image" class="leader-img"/>
+          <prismic-rich-text :field="type.name" class="leader-name"/>
+        </label>
+        
+        <div class="collapsible-content">
+          <div class="content-inner">
+            <prismic-rich-text :field="type.description"/>
+          </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue, { ComputedOptions } from 'vue';
+import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Leaders',
-  props: {
-    msg: String,
-  },
   data() {
     return {
       document: null
